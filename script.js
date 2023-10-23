4ade91a8c2f18764a9061dd3cb1842ed5a70f8d6
Simran Gupta <simrangupta172002@gmail.com>
2023-10-23 13:59:06 +0530

    made page2 responsive

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var colorButtons = document.querySelectorAll(".button");
+  var colorButtons = document.querySelectorAll(".button");
 
-    colorButtons.forEach(function(button) {
-        button.addEventListener("click", function () {
-            // Remove the 'color-changed' class from all buttons
-            colorButtons.forEach(function(btn) {
-                btn.classList.remove("button-color");
-            });
+  colorButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // Remove the 'color-changed' class from all buttons
+      colorButtons.forEach(function (btn) {
+        btn.classList.remove("button-color");
+      });
 
-            // Add the 'color-changed' class to the clicked button
-            this.classList.add("button-color");
-        });
+      // Add the 'color-changed' class to the clicked button
+      this.classList.add("button-color");
     });
+  });
 });
 
 function changeText(newText) {
-    var mainHeading = document.getElementById("main-heading");
-    mainHeading.textContent = newText;
+  var mainHeading = document.getElementById("main-heading");
+  mainHeading.textContent = newText;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var colorButtons = document.querySelectorAll(".button2");
+  var colorButtons = document.querySelectorAll(".button2");
 
-    colorButtons.forEach(function(button2) {
-        button2.addEventListener("click", function () {
-            // Remove the 'color-changed' class from all buttons
-            colorButtons.forEach(function(btn) {
-                btn.classList.remove("button-color");
-            });
+  colorButtons.forEach(function (button2) {
+    button2.addEventListener("click", function () {
+      // Remove the 'color-changed' class from all buttons
+      colorButtons.forEach(function (btn) {
+        btn.classList.remove("button-color");
+      });
 
-            // Add the 'color-changed' class to the clicked button
-            this.classList.add("button-color");
-        });
+      // Add the 'color-changed' class to the clicked button
+      this.classList.add("button-color");
     });
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var videobtn = document.getElementById("video");
+  var thumbnailbtn = document.getElementById("thumbnail");
+  var videodiv = document.getElementById("videodiv");
+  var thumbnaildiv = document.getElementById("thumbnaildiv");
+  videobtn.addEventListener("click", function () {
+    videodiv.style.display = "block"; // Show Table 1
+    thumbnaildiv.style.display = "none"; // Hide Table 2
+  });
+  thumbnailbtn.addEventListener("click", function () {
+    thumbnaildiv.style.display = "block"; // Show Table 1
+    videodiv.style.display = "none"; // Hide Table 2
+  });
 });
